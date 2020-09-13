5e19b0fa8f98edc4e15379e6ccd1c68bad41b92b
Adam Zerella <hi@adamzerella.com>
2020-09-13 16:38:22 +0930

    Wrong package import for new package

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 
